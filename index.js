44d260b9d1201791d7412840e54917ecdb82916c
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-02 20:25:11 +0600

    Added delete specific  data  router function

--- index.js
+++ index.js
@@ -57,6 +57,13 @@ async function run() {
       const orders = await cursor.toArray();
       res.send(orders);
     });
+
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
