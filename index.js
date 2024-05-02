4eab1acf1c67ea2b9abce8c0c54cdb02f6c30f1d
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-02 20:48:55 +0600

    Added booking confirm function

--- index.js
+++ index.js
@@ -58,6 +58,20 @@ async function run() {
       res.send(orders);
     });
 
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedOrder = req.body;
+
+      const updateDoc = {
+        $set: {
+          status: updatedOrder.status,
+        },
+      };
+      const result = await orderCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     app.delete("/orders/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
